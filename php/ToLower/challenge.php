209056c241c1edc48038d0f623f84f44afed8be9
Michael Gorianskyi <michael.gorianskyi@westwing.de>
2016-01-05 15:06:17 +0100

    Rearrangement

--- php/ToLower/challenge.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('../common/Cli.php');
-require_once('../common/File.php');
-require_once('../common/ChallengeAbstract.php');
-require_once('ToLower.php');
-
-new ToLower();
---
+++ php/easy/Lowercase/challenge.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('../../common/Cli.php');
+require_once('../../common/File.php');
+require_once('../../common/ChallengeAbstract.php');
+require_once('ToLower.php');
+
+new ToLower();
